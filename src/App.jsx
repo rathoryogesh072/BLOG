d84a6183b4835e792504225aa8620ca2708e0902
rathoryogesh072 <rathoryogesh072@gmail.com>
2023-09-27 18:07:40 +0530

    redux auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
+import { useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import authService from "./appwrite/auth"
+import { logout,login } from './store/authSlice'
+import {Footer,Header} from './components'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-    <h1>A blog app with appwrite</h1>
-    </>
-  )
+  const [loading,setLoading]=useState(true)
+  const dispatch=useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout());
+      }
+    })
+    .finally(()=>setLoading(false))
+  },[])
+  return !loading?(
+    <div className='bg-gray-400'>
+      <div>
+        <Header/>
+        <Footer/>
+      </div>
+    </div>
+
+  ):null
 }
 
 export default App
